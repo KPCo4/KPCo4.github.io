ca49cde45e1cf5fc3e4802a9bc723f93957e60ee
KPCo4 <83274776+KPCo4@users.noreply.github.com>
2025-11-30 16:11:44 +0000

    added open source page analytics

---
+++ analytics.js
@@ -0,0 +1,8 @@
+// Load GoatCounter
+(function() {
+    var script = document.createElement('script');
+    script.dataset.goatcounter = "https://kyepc.goatcounter.com/count";
+    script.async = true;
+    script.src = "//gc.zgo.at/count.js";
+    document.head.appendChild(script);
+})();
